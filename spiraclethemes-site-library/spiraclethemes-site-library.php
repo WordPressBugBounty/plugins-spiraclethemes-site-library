dbd9c62d3a3ab1903a86036263e6ee082b5a53ce
Sean Murphy <sean@iamseanmurphy.com>
2025-05-24 09:17:27 +0000

    1.5.2

--- spiraclethemes-site-library/spiraclethemes-site-library.php
+++ spiraclethemes-site-library/spiraclethemes-site-library.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Spiraclethemes Site Library
  * Plugin URI:        https://wordpress.org/plugins/spiraclethemes-site-library/
  * Description:       A plugin by Spiracle Themes that adds one-click demo import, theme customization, starter templates, and page builder support to its free themes.
- * Version:           1.5.1
+ * Version:           1.5.2
  * Author:            SpiracleThemes
  * Author URI:        https://spiraclethemes.com
  * License:           GPL-2.0+
@@ -66,7 +66,32 @@ class Spiraclethemes_Site_Library {
         $this->theme_slug    = $theme->get( 'TextDomain' );
         $this->theme_version = $theme->get( 'Version' );
 
-        if ( is_admin() ) {
+        // Define allowed Spiraclethemes theme slugs
+        $allowed_themes = [
+            'own-shop',
+            'purea-magazine',
+            'colon',
+            'somalite',
+            'purea-fashion',
+            'own-store',
+            'colon-plus',
+            'own-shop-lite',
+            'mestore',
+            'blogson',
+            'blogson-child',
+            'own-shope',
+            'crater-free',
+            'lawfiz',
+            'legalblow',
+            'own-shop-trend',
+            'lawfiz-one',
+            'krystal',
+            'krystal-lawyer',
+            'krystal-business',
+            'krystal-shop'
+        ];
+
+        if (is_admin() && in_array($this->theme_slug, $allowed_themes)) {
             add_action( 'admin_init', [ $this, 'spiraclethemes_site_library_set_notification' ] );
             add_action( 'admin_notices', [ $this, 'spiraclethemes_site_library_display_welcome_notice' ] );
             add_action( 'admin_notices', [ $this, 'spiraclethemes_site_library_display_sale10_notice' ] );
@@ -77,7 +102,6 @@ class Spiraclethemes_Site_Library {
             add_action( 'admin_init', [ $this, 'spiraclethemes_site_library_ignore_sale20_notice' ] );
             add_action( 'admin_init', [ $this, 'spiraclethemes_site_library_ignore_sale40_notice' ] );
         }
-
         add_action('init', [ $this, 'spiraclethemes_site_library_load_plugin_textdomain' ] );
     }
 
@@ -148,25 +172,25 @@ class Spiraclethemes_Site_Library {
     }
 
     // Reusable method to build notice message
-    private function spiraclethemes_site_library_build_sale_notice( $discount, $code, $ignore_param ) {
+    private function spiraclethemes_site_library_build_sale_notice( $discount, $code, $ignore_param, $theme_name ) {
         $pricing_url = esc_url( 'https://spiraclethemes.com/pricing/' );
         $ignore_url  = esc_url( admin_url( 'themes.php?' . $ignore_param . '=0' ) );
 
         switch ( $discount ) {
             case 10:
-                $message = __( '🎉 Congratulations! You\'re eligible for an exclusive 10%% discount on our PRO upgrade. Use promotional code <strong>%1$s</strong> at checkout. Visit <a href="%2$s" target="_blank">our pricing page</a> to learn more. <a href="%3$s"> | No thanks</a>', 'spiraclethemes-site-library' );
+                $message = __( '🎉 Congratulations! You\'re eligible for an exclusive 10%% discount on %4$s PRO upgrade. Use promotional code <strong>%1$s</strong> at checkout. Visit <a href="%2$s" target="_blank">our pricing page</a> to learn more. <a href="%3$s"> | No thanks</a>', 'spiraclethemes-site-library' );
                 break;
             case 20:
-                $message = __( '🎉 Great news! You\'ve unlocked a 20%% discount on our PRO upgrade. Use promotional code <strong>%1$s</strong> at checkout. Visit <a href="%2$s" target="_blank">our pricing page</a> to learn more. <a href="%3$s"> | No thanks</a>', 'spiraclethemes-site-library' );
+                $message = __( '🎉 Great news! You\'ve unlocked a 20%% discount on %4$s PRO upgrade. Use promotional code <strong>%1$s</strong> at checkout. Visit <a href="%2$s" target="_blank">our pricing page</a> to learn more. <a href="%3$s"> | No thanks</a>', 'spiraclethemes-site-library' );
                 break;
             case 40:
-                $message = __( '🎉 Final chance! A massive 40%% discount awaits you on our PRO upgrade. Use promotional code <strong>%1$s</strong> at checkout. Visit <a href="%2$s" target="_blank">our pricing page</a> to learn more. <a href="%3$s"> | No thanks</a>', 'spiraclethemes-site-library' );
+                $message = __( '🎉 Final chance! A massive 40%% discount awaits you on %4$s PRO upgrade. Use promotional code <strong>%1$s</strong> at checkout. Visit <a href="%2$s" target="_blank">our pricing page</a> to learn more. <a href="%3$s"> | No thanks</a>', 'spiraclethemes-site-library' );
                 break;
             default:
                 return '';
         }
 
-        return sprintf( wp_kses_post( $message ), esc_html( $code ), $pricing_url, $ignore_url );
+        return sprintf( wp_kses_post( $message ), esc_html( $code ), $pricing_url, $ignore_url, $theme_name );
     }
 
 
@@ -210,7 +234,7 @@ class Spiraclethemes_Site_Library {
 
         if ( $days_since >= 7 && $days_since < 14 &&
              $this->spiraclethemes_site_library_should_display_notice( 'spiraclethemes_sitelib_sale10_ignore_notice', 7 ) ) {
-            $message = $this->spiraclethemes_site_library_build_sale_notice( 10, 'SALE10', 'wp_spiraclethemes_sitelib_sale10_ignore' );
+            $message = $this->spiraclethemes_site_library_build_sale_notice( 10, 'SALE10', 'wp_spiraclethemes_sitelib_sale10_ignore', $this->theme_name );
             $this->spiraclethemes_site_library_display_custom_notice( $message );
         }
     }
@@ -220,7 +244,7 @@ class Spiraclethemes_Site_Library {
 
         if ( $days_since >= 14 && $days_since < 28 &&
              $this->spiraclethemes_site_library_should_display_notice( 'spiraclethemes_sitelib_sale20_ignore_notice', 14 ) ) {
-            $message = $this->spiraclethemes_site_library_build_sale_notice( 20, '20SALEOFF', 'wp_spiraclethemes_sitelib_sale20_ignore' );
+            $message = $this->spiraclethemes_site_library_build_sale_notice( 20, '20SALEOFF', 'wp_spiraclethemes_sitelib_sale20_ignore', $this->theme_name );
             $this->spiraclethemes_site_library_display_custom_notice( $message );
         }
     }
@@ -230,7 +254,7 @@ class Spiraclethemes_Site_Library {
 
         if ( $days_since >= 28 &&
              $this->spiraclethemes_site_library_should_display_notice( 'spiraclethemes_sitelib_sale40_ignore_notice', 28 ) ) {
-            $message = $this->spiraclethemes_site_library_build_sale_notice( 40, 'SALE40', 'wp_spiraclethemes_sitelib_sale40_ignore' );
+            $message = $this->spiraclethemes_site_library_build_sale_notice( 40, 'SALE40', 'wp_spiraclethemes_sitelib_sale40_ignore', $this->theme_name );
             $this->spiraclethemes_site_library_display_custom_notice( $message );
         }
     }
